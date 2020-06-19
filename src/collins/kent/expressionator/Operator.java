2fba195a588d8c965c13fdbdeca744f9424d7156
Kent Collins <1175379+kentcollins@users.noreply.github.com>
2020-06-19 23:00:59 +0100

    generating problems

--- src/collins/kent/expressionator/Operator.java
+++ src/collins/kent/expressionator/Operator.java
@@ -1,4 +1,9 @@
 package collins.kent.expressionator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 /***
  * Operations that may be performed on values in the Expressionator.
  * 
@@ -9,5 +14,60 @@ public enum Operator {
 
 	ADD, SUB, MULT, DIV, MOD, AND, OR, LT, LTE, GT, GTE, EQUAL, ASSIGN, ADD_ASSIGN, DIV_ASSIGN, MULT_ASSIGN, SUB_ASSIGN,
 	MOD_ASSIGN;
+
+	public static final List<Operator> LOGICAL = Arrays.asList(new Operator[] { AND, OR });
+	public static final List<Operator> ARITHMETIC = Arrays.asList(new Operator[] { ADD, SUB, MULT, DIV, MOD });
+	public static final List<Operator> COMPOUND = Arrays
+			.asList(new Operator[] { ADD_ASSIGN, DIV_ASSIGN, MULT_ASSIGN, SUB_ASSIGN, MOD_ASSIGN });
+	public static final List<Operator> COMPARISON = Arrays.asList(new Operator[] { LT, LTE, GT, GTE, EQUAL });
+	
+	public static Random random = new Random();
+
+	static Operator random(List<Operator> list) {
+		return list.get(random.nextInt(list.size()));
+	}
+	
+	static String symbol(Operator op) {
+		switch (op) {
+		case ADD:
+			return "+";
+		case SUB:
+			return "-";
+		case MULT:
+			return "*";
+		case DIV:
+			return "/";
+		case MOD:
+			return "%";
+		case AND:
+			return "&&";
+		case OR:
+			return "||";
+		case LT:
+			return "<";
+		case LTE:
+			return "<=";
+		case GT:
+			return ">";
+		case GTE:
+			return ">=";
+		case EQUAL:
+			return "==";
+		case ASSIGN:
+			return "=";
+		case ADD_ASSIGN:
+			return "+=";
+		case SUB_ASSIGN:
+			return "-=";
+		case MULT_ASSIGN:
+			return "*=";
+		case DIV_ASSIGN:
+			return "/=";
+		case MOD_ASSIGN:
+			return "%=";
+		default:
+			return "NO SIGN FOUND";
+		}
+	}
 
 }
