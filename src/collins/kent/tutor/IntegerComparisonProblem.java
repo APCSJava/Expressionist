f70514feb5dc2fed5bb751e7d1851b793f6be086
Kent Collins <1175379+kentcollins@users.noreply.github.com>
2020-06-21 15:02:37 +0100

    fixes #13

--- src/collins/kent/tutor/IntegerComparisonProblem.java
+++ src/collins/kent/tutor/IntegerComparisonProblem.java
@@ -1,5 +1,48 @@
 package collins.kent.tutor;
 
+import static collins.kent.tutor.Operator.*;
+
 public class IntegerComparisonProblem extends Problem {
 
+	int leftOperand;
+	int rightOperand;
+	Operator op;
+
+	public IntegerComparisonProblem() {
+		this(Operator.random(COMPARISON));
+	}
+
+	public IntegerComparisonProblem(Operator op) {
+		leftOperand = random.nextInt(5);
+		// provide slight bias towards same so GTE, LTE and == are relevant
+		rightOperand = random.nextDouble() < 0.2 ? leftOperand : random.nextInt(5);
+		this.statement = leftOperand + " " + Operator.symbol(op) + " " + rightOperand;
+		this.answer = result(op);
+		this.correction = "Incorrect.  The comparison is " + answer;
+	}
+
+	private String result(Operator op) {
+		boolean result = false;
+		switch (op) {
+		case LTE:
+			result = leftOperand <= rightOperand;
+			break;
+		case LT:
+			result = leftOperand < rightOperand;
+			break;
+		case GT:
+			result = leftOperand > rightOperand;
+			break;
+		case GTE:
+			result = leftOperand >= rightOperand;
+			break;
+		case EQUAL:
+			result = leftOperand == rightOperand;
+			break;
+		case NOT_EQUAL:
+			result = leftOperand != rightOperand;
+			break;
+		}
+		return Boolean.toString(result);
+	}
 }
