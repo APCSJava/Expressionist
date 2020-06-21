7e0753dd17588444d56ddb8daf1b0e05b1a36e82
Kent Collins <1175379+kentcollins@users.noreply.github.com>
2020-06-21 09:10:03 +0100

    added getter/setter even though default visibility

--- src/collins/kent/tutor/Problem.java
+++ src/collins/kent/tutor/Problem.java
@@ -17,13 +17,34 @@ public abstract class Problem {
 	boolean isCorrect(String response) {
 		return answer.equals(response);
 	}
-	
-	String getCorrection() {
-		return correction;
-	}
-	
+
+	@Override
 	public String toString() {
 		return statement;
+	}
+
+	public String getStatement() {
+		return statement;
+	}
+
+	public void setStatement(String statement) {
+		this.statement = statement;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public void setCorrection(String correction) {
+		this.correction = correction;
+	}
+
+	String getCorrection() {
+		return correction;
 	}
 	
 }
