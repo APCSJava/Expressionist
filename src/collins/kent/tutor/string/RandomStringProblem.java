8786b0a737b41910b5bdc358afb73774ba37b770
Kent Collins <1175379+kentcollins@users.noreply.github.com>
2020-06-24 21:22:49 +0100

    storing classes in array for random selection

--- src/collins/kent/tutor/string/RandomStringProblem.java
+++ src/collins/kent/tutor/string/RandomStringProblem.java
@@ -7,32 +7,26 @@ import collins.kent.tutor.Problem;
 public class RandomStringProblem implements Problem {
 
 	Problem actual;
+	Class[] classes = { StringIndexOfProblem.class,
+			StringLengthProblem.class,
+			SubstringIndexErrorProblem.class,
+			SubstringLengthSingleArgumentProblem.class,
+			SubstringLengthTwoArgumentProblem.class,
+			SubstringSingleIndexProblem.class,
+			SubstringTwoIndexProblem.class
+			};
 
 	@Override
 	public Problem generate(Random rng) {
-		int r = rng.nextInt(7);
-		switch (r) {
-		case 0:
-			actual = new StringIndexOfProblem();
-			break;
-		case 1:
-			actual = new StringLengthProblem();
-			break;
-		case 2:
-			actual = new SubstringIndexErrorProblem();
-			break;
-		case 3:
-			actual = new SubstringLengthSingleArgumentProblem();
-			break;
-		case 4:
-			actual = new SubstringLengthTwoArgumentProblem();
-			break;
-		case 5:
-			actual = new SubstringSingleIndexProblem();
-			break;
-		case 6:
-			actual = new SubstringTwoIndexProblem();
-			break;
+		int selection = rng.nextInt(classes.length);
+		try {
+			actual = (Problem) classes[selection].newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		actual.generate(rng);
 		return this;
