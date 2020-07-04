9c480521b35e2855f14c349bd02dd6e00710f694
Kent Collins <1175379+kentcollins@users.noreply.github.com>
2020-07-04 10:02:27 +0100

    fixes #12

---
+++ src/collins/kent/tutor/comparison/GotchaDoubleEqualityCheckProblem.java
@@ -0,0 +1,42 @@
+package collins.kent.tutor.comparison;
+
+import java.util.Random;
+
+import collins.kent.tutor.Operator;
+import collins.kent.tutor.Problem;
+
+public class GotchaDoubleEqualityCheckProblem implements Problem {
+
+	String s1 = "0.1 + 0.2 == 0.3"; // false
+	String s2 = "0.3 == 0.1 + 0.2"; // false
+	String s3 = "0.1 + 0.2 <= 0.3"; // false
+	String s4 = "0.1 + 0.2 != 0.3"; // true
+	String s5 = "0.3 < 0.1 + 0.2"; // true
+	String[] options = { s1, s2, s3, s4, s5 };
+	int choice;
+	String f1 = "An explanation is appropriate. This expression evaluates false.  Unlike integers, doubles are inexact values. Use comparison operators carefully.";
+	String f2 = "An explanation is appropriate. This expression evaluates true.  Unlike integers, doubles are inexact values. Use comparison operators carefully.";
+	String[] feedback = { f1, f1, f1, f2, f2 };
+
+	@Override
+	public Problem generate(Random rng) {
+		choice = rng.nextInt(options.length);
+		return this;
+	}
+
+	@Override
+	public String getStatement() {
+		return options[choice];
+	}
+
+	@Override
+	public String getAnswer() {
+		return "e";
+	}
+
+	@Override
+	public String getFeedback(String s) {
+		return feedback[choice];
+	}
+
+}
