f3c2b4e1fc203551ff298d8f0cb9531a9dd906c0
Kent Collins <1175379+kentcollins@users.noreply.github.com>
2021-09-16 16:48:16 +0100

    Shows question number of generated test

--- src/collins/kent/tutor/TestGenerator.java
+++ src/collins/kent/tutor/TestGenerator.java
@@ -15,122 +15,123 @@ import java.util.Scanner;
 /***
  * Creates drills to strengthen student reasoning about expressions across
  * different data type and operators.
- * 
+ *
  * @author k. collins
  *
  */
 
 public class TestGenerator {
 
-	public static final String ERROR = "e";
+    public static final String ERROR = "e";
 
-	static final Scanner scanner = new Scanner(System.in);
-	ArrayList<Problem> toAsk = new ArrayList<>();
-	ArrayList<Problem> correct = new ArrayList<>();
-	ArrayList<Problem> toReview = new ArrayList<>();
-	ArrayList<Problem> reviewed = new ArrayList<>(); // will hold all that were
-														// incorrect
-	Random random = new Random(); // central source for random numbers
-	int numMissed = 0;
+    static final Scanner scanner = new Scanner(System.in);
+    ArrayList<Problem> toAsk = new ArrayList<>();
+    ArrayList<Problem> correct = new ArrayList<>();
+    ArrayList<Problem> toReview = new ArrayList<>();
+    ArrayList<Problem> reviewed = new ArrayList<>(); // will hold all that were
+    // incorrect
+    Random random = new Random(); // central source for random numbers
+    int numMissed = 0;
 
-	public void ask(Problem p) {
-		System.out.println(p.getStatement());
-		String response = scanner.next().trim();
-		boolean isCorrect = p.isCorrect(response);
-		if (isCorrect) {
-			System.out.println("Correct\n");
-		} else {
-			System.out.println("*****  INCORRECT  *****\t"
-					+ p.getFeedback(response)+"\n");
-			numMissed++;
-			toReview.add(p);
-		}
-	}
+    public static void main(String[] args) {
+        TestGenerator tutor = new TestGenerator();
+        // tutor.random.setSeed(1L);
+        System.out.println("Please enter your name: ");
+        String name = scanner.nextLine().trim();
 
-	public static void main(String[] args) {
-		TestGenerator tutor = new TestGenerator();
-		// tutor.random.setSeed(1L);
-		System.out.println("Please enter your name: ");
-		String name = scanner.nextLine().trim();
+        for (int i = 0; i < 3; i++) {
+            tutor.toAsk.add(new DoubleAdditionProblem().generate(tutor.random));
+            tutor.toAsk.add(new DoubleDivisionProblem().generate(tutor.random));
+            tutor.toAsk.add(new DoubleMultiplicationProblem().generate(tutor.random));
+            tutor.toAsk.add(new DoubleSubtractionProblem().generate(tutor.random));
+            tutor.toAsk.add(new IntegerAdditionProblem().generate(tutor.random));
+            tutor.toAsk.add(new IntegerDivisionByZeroProblem().generate(tutor.random));
+            tutor.toAsk.add(new IntegerDivisionProblem().generate(tutor.random));
+            tutor.toAsk.add(new IntegerModuloProblem().generate(tutor.random));
+            tutor.toAsk.add(new IntegerModuloZeroProblem().generate(tutor.random));
+            tutor.toAsk.add(new IntegerMultiplicationProblem().generate(tutor.random));
+            tutor.toAsk.add(new IntegerSubtractionProblem().generate(tutor.random));
+            tutor.toAsk.add(new MixedTypeArithmeticProblem().generate(tutor.random));
+            tutor.toAsk.add(new CastDoubleDividendToIntProblem().generate(tutor.random));
+            tutor.toAsk.add(new CastIntDividendToDoubleProblem().generate(tutor.random));
+            tutor.toAsk.add(new GotchaOperatorNotLiteralProblem().generate(tutor.random));
+            tutor.toAsk.add(new GotchaStringNotBooleanProblem().generate(tutor.random));
+            tutor.toAsk.add(new GotchaStringNotCharProblem().generate(tutor.random));
+            tutor.toAsk.add(new GotchaStringNotNumericProblem().generate(tutor.random));
+            tutor.toAsk.add(new GotchaVariableNotLiteralProblem().generate(tutor.random));
+            tutor.toAsk.add(new RecognizeBooleanLiteralProblem().generate(tutor.random));
+            tutor.toAsk.add(new RecognizeCharLiteralProblem().generate(tutor.random));
+            tutor.toAsk.add(new RecognizeDoubleLiteralProblem().generate(tutor.random));
+            tutor.toAsk.add(new RecognizeFloatLiteralProblem().generate(tutor.random));
+            tutor.toAsk.add(new RecognizeIntLiteralProblem().generate(tutor.random));
+            tutor.toAsk.add(new RecognizeLongLiteralProblem().generate(tutor.random));
+            tutor.toAsk.add(new RecognizeStringLiteralProblem().generate(tutor.random));
+            tutor.toAsk.add(new IntegerComparisonProblem().generate(tutor.random));
+            tutor.toAsk.add(new DoubleComparisonProblem().generate(tutor.random));
+            tutor.toAsk.add(new GotchaAssignVersusCompareProblem().generate(tutor.random));
+            tutor.toAsk.add(new GotchaDoubleEqualityCheckProblem().generate(tutor.random));
+            tutor.toAsk.add(new GotchaIntegerComparisonProblem().generate(tutor.random));
+        }
+        // seven more - focus on the gotchas
+        tutor.toAsk.add(new GotchaAssignVersusCompareProblem().generate(tutor.random));
+        tutor.toAsk.add(new GotchaIntegerComparisonProblem().generate(tutor.random));
+        tutor.toAsk.add(new GotchaOperatorNotLiteralProblem().generate(tutor.random));
+        tutor.toAsk.add(new GotchaStringNotBooleanProblem().generate(tutor.random));
+        tutor.toAsk.add(new GotchaStringNotCharProblem().generate(tutor.random));
+        tutor.toAsk.add(new GotchaStringNotNumericProblem().generate(tutor.random));
+        tutor.toAsk.add(new GotchaVariableNotLiteralProblem().generate(tutor.random));
 
-		for (int i = 0; i<3; i++) {
-			tutor.toAsk.add(new DoubleAdditionProblem().generate(tutor.random));
-			tutor.toAsk.add(new DoubleDivisionProblem().generate(tutor.random));
-			tutor.toAsk.add(new DoubleMultiplicationProblem().generate(tutor.random));
-			tutor.toAsk.add(new DoubleSubtractionProblem().generate(tutor.random));
-			tutor.toAsk.add(new IntegerAdditionProblem().generate(tutor.random));
-			tutor.toAsk.add(new IntegerDivisionByZeroProblem().generate(tutor.random));
-			tutor.toAsk.add(new IntegerDivisionProblem().generate(tutor.random));
-			tutor.toAsk.add(new IntegerModuloProblem().generate(tutor.random));
-			tutor.toAsk.add(new IntegerModuloZeroProblem().generate(tutor.random));
-			tutor.toAsk.add(new IntegerMultiplicationProblem().generate(tutor.random));
-			tutor.toAsk.add(new IntegerSubtractionProblem().generate(tutor.random));
-			tutor.toAsk.add(new MixedTypeArithmeticProblem().generate(tutor.random));
-			tutor.toAsk.add(new CastDoubleDividendToIntProblem().generate(tutor.random));
-			tutor.toAsk.add(new CastIntDividendToDoubleProblem().generate(tutor.random));
-			tutor.toAsk.add(new GotchaOperatorNotLiteralProblem().generate(tutor.random));
-			tutor.toAsk.add(new GotchaStringNotBooleanProblem().generate(tutor.random));
-			tutor.toAsk.add(new GotchaStringNotCharProblem().generate(tutor.random));
-			tutor.toAsk.add(new GotchaStringNotNumericProblem().generate(tutor.random));
-			tutor.toAsk.add(new GotchaVariableNotLiteralProblem().generate(tutor.random));
-			tutor.toAsk.add(new RecognizeBooleanLiteralProblem().generate(tutor.random));
-			tutor.toAsk.add(new RecognizeCharLiteralProblem().generate(tutor.random));
-			tutor.toAsk.add(new RecognizeDoubleLiteralProblem().generate(tutor.random));
-			tutor.toAsk.add(new RecognizeFloatLiteralProblem().generate(tutor.random));
-			tutor.toAsk.add(new RecognizeIntLiteralProblem().generate(tutor.random));
-			tutor.toAsk.add(new RecognizeLongLiteralProblem().generate(tutor.random));
-			tutor.toAsk.add(new RecognizeStringLiteralProblem().generate(tutor.random));
-			tutor.toAsk.add(new IntegerComparisonProblem().generate(tutor.random));
-			tutor.toAsk.add(new DoubleComparisonProblem().generate(tutor.random));
-			tutor.toAsk.add(new GotchaAssignVersusCompareProblem().generate(tutor.random));
-			tutor.toAsk.add(new GotchaDoubleEqualityCheckProblem().generate(tutor.random));
-			tutor.toAsk.add(new GotchaIntegerComparisonProblem().generate(tutor.random));
-		}
-		// seven more - focus on the gotchas
-		tutor.toAsk.add(new GotchaAssignVersusCompareProblem().generate(tutor.random));
-		tutor.toAsk.add(new GotchaIntegerComparisonProblem().generate(tutor.random));
-		tutor.toAsk.add(new GotchaOperatorNotLiteralProblem().generate(tutor.random));
-		tutor.toAsk.add(new GotchaStringNotBooleanProblem().generate(tutor.random));
-		tutor.toAsk.add(new GotchaStringNotCharProblem().generate(tutor.random));
-		tutor.toAsk.add(new GotchaStringNotNumericProblem().generate(tutor.random));
-		tutor.toAsk.add(new GotchaVariableNotLiteralProblem().generate(tutor.random));
+        Collections.shuffle(tutor.toAsk);
 
-		Collections.shuffle(tutor.toAsk);
+        System.out.println(tutor.toAsk.size() + " questions await.  Give the correct answer or 'e' (without quotes) in situations where an error is present or that would benefit from an explanation.");
+        for (int i = 0; i < tutor.toAsk.size(); i++) {
+            System.out.println("Question " + (i + 1) + ".");
+            tutor.ask(tutor.toAsk.get(i));
+        }
+        System.out.println(name + " completed the assessment at "
+                + LocalDateTime.now() + ".\n" + "Number Missed: "
+                + (tutor.numMissed) + " out of " + tutor.toAsk.size());
+        if (tutor.numMissed > 0) {
+            System.out.println("Missed question types: ");
+            for (Problem p : tutor.toReview) {
+                System.out.println("\t" + p.getClass().getSimpleName());
+            }
+        }
+    }
 
-		System.out.println(tutor.toAsk.size()+ " questions await.  Give the correct answer or 'e' (without quotes) in situations where an error is present or that would benefit from an explanation.");
-		for (int i = 0; i < tutor.toAsk.size(); i++) {
-			tutor.ask(tutor.toAsk.get(i));
-		}
-		System.out.println(name + " completed the assessment at "
-				+ LocalDateTime.now() + ".\n" + "Number Missed: "
-				+ (tutor.numMissed) +" out of "+tutor.toAsk.size());
-		if (tutor.numMissed>0) {
-			System.out.println("Missed question types: ");
-			for (Problem p:tutor.toReview) {
-				System.out.println("\t"+p.getClass().getSimpleName());
-			}
-		}
-	}
+    public static String getExceptionSymbol() {
+        return "e";
+    }
 
-	public void addToReview(Problem p, Random rand) {
-		Class<? extends Problem> c = p.getClass();
-		//System.out.println("Missed: " + p.getClass());
-		//System.out.println("Adding to review: " + c);
-		try {
-			toReview.add(c.newInstance().generate(rand));
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
-	}
+    public static String getErrorSymbol() {
+        return "e";
+    }
 
-	public static String getExceptionSymbol() {
-		return "e";
-	}
+    public void ask(Problem p) {
+        System.out.println(p.getStatement());
+        String response = scanner.next().trim();
+        boolean isCorrect = p.isCorrect(response);
+        if (isCorrect) {
+            System.out.println("Correct\n");
+        } else {
+            System.out.println("*****  INCORRECT  *****\t"
+                    + p.getFeedback(response) + "\n");
+            numMissed++;
+            toReview.add(p);
+        }
+    }
 
-	public static String getErrorSymbol() {
-		return "e";
-	}
+    public void addToReview(Problem p, Random rand) {
+        Class<? extends Problem> c = p.getClass();
+        //System.out.println("Missed: " + p.getClass());
+        //System.out.println("Adding to review: " + c);
+        try {
+            toReview.add(c.newInstance().generate(rand));
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
